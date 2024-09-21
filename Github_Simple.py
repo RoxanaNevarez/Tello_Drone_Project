bca01d39ed18cb97356fc12222cdb3208b670aa0
Roxana Nevarez <80090572+RoxanaNevarez@users.noreply.github.com>
2024-09-21 00:23:23 -0700

    Update Github_Simple.py

--- Github_Simple.py
+++ Github_Simple.py
@@ -6,6 +6,19 @@ from djitellopy import Tello
 # tello variable is assigned to Tello class from tello.py
 tello = Tello()
 
+#everything below this point has to do with knowing dot notation
+#for dot notation, that requires knowing classes and user-defined functions
+
+#recall syntax for classes:
+
+#class <class_definition>(object):
+     #perform logic below
+
+#recall syntax for user-defined functions: 
+
+#def <function_name>(<parameters:optional>):
+     #perform logic below
+
 # tello will connect to PyCharm and will be ready to receive any incoming code
 tello.connect()
 
