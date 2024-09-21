13b26442e29307914829bb3edef1ebbd2985aba6
Roxana Nevarez <80090572+RoxanaNevarez@users.noreply.github.com>
2024-09-21 00:24:16 -0700

    Update Github_Simple.py

--- Github_Simple.py
+++ Github_Simple.py
@@ -1,22 +1,23 @@
 #Early stages of learning Python programming to control drone
 
+# recall syntax to import a function from a library: from <library> import <function>
 # imports Tello commands from djitellopy library
 from djitellopy import Tello
 
 # tello variable is assigned to Tello class from tello.py
 tello = Tello()
 
-#everything below this point has to do with knowing dot notation
-#for dot notation, that requires knowing classes and user-defined functions
+# everything below this point has to do with knowing dot notation
+# for dot notation, that requires knowing classes and user-defined functions
 
-#recall syntax for classes:
+# recall syntax for classes:
 
-#class <class_definition>(object):
+# class <class_definition>(object):
      #perform logic below
 
-#recall syntax for user-defined functions: 
+# recall syntax for user-defined functions: 
 
-#def <function_name>(<parameters:optional>):
+# def <function_name>(<parameters:optional>):
      #perform logic below
 
 # tello will connect to PyCharm and will be ready to receive any incoming code
